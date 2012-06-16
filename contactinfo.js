07f925e010872556ae699c2f26b0362d50dd101e
Daniel Boulet <dan@danboulet.com>
2012-06-16 15:10:19 -0500

    New feature: ability to use site slogan as tagline.

--- contactinfo.js
+++ contactinfo.js
@@ -5,22 +5,35 @@
 (function ($) {
   Drupal.behaviors.contactinfo = {
     attach: function() {
-      var useSiteNameCheckbox = $('#edit-contactinfo-use-site-name');
-      var orgTextField = $('#edit-contactinfo-org');
-      var siteName = Drupal.settings.siteName;
-      var organizationName = orgTextField.val();
-
-      if (useSiteNameCheckbox.is(':checked')) {
-        orgTextField.attr('disabled', 'disabled').val(siteName);
+      var contactInfoProperties = {
+        org: {
+          $checkbox: $('#edit-contactinfo-use-site-name'),
+          $textField: $('#edit-contactinfo-org'),
+          siteSettingsVal: Drupal.settings.siteName
+        },
+        tagline: {
+          $checkbox: $('#edit-contactinfo-use-site-slogan'),
+          $textField: $('#edit-contactinfo-tagline'),
+          siteSettingsVal: Drupal.settings.siteSlogan
+        }
       }
-      useSiteNameCheckbox.change(function() {
-        if ($(this).is(':checked')) {
-          organizationName = orgTextField.val();
-          orgTextField.attr('disabled', 'disabled').val(siteName);
+
+      $.each(contactInfoProperties, function (i, v) {
+        // Store user-entered value.
+        var userEnteredVal = v.$textField.val();
+        if (v.$checkbox.is(':checked')) {
+          v.$textField.attr('disabled', 'disabled').val(v.siteSettingsVal);
         }
-        else {
-          orgTextField.removeAttr('disabled').val(organizationName);
-        }
+        v.$checkbox.change(function () {
+          if ($(this).is(':checked')) {
+            // Store latest user-entered value in case the checkbox gets unchecked.
+            userEnteredVal = v.$textField.val();
+            v.$textField.attr('disabled', 'disabled').val(v.siteSettingsVal);
+          }
+          else {
+            v.$textField.removeAttr('disabled').val(userEnteredVal);
+          }
+        });
       });
     }
   };
